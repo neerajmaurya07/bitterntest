8b0d6440e6abff41b08e0d4e66e8bc55c1d4c48b
rakesh kumar <rakeshmaurya262@gmail.com>
2017-03-04 17:01:37 +0530

    all done

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,13 @@
 class Blog < ApplicationRecord
   belongs_to :category
- 	has_many :comments
+  has_many :comments
+  mount_uploader :image, BlogImageUploader
+
+  def self.search_title(search)
+	  if search
+  		where("lower(title) Like ?", "%#{search.downcase}%")
+	  else
+	    all
+	  end
+	end
 end
