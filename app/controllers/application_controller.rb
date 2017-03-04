f3dea2389986b37a77a60fe99017010f083433b3
satishsehgal <satish.k6677@gmail.com>
2017-03-04 18:27:12 +0530

    root page done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create,:show, :edit, :update, :destroy]
   helper_method :admin_user
   
   def admin_user
