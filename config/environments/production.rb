ee994441bf908ea5aaa50da7d5a845abdde9190a
satishsehgal <satish.k6677@gmail.com>
2017-03-06 12:11:32 +0530

    changes done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'bitterntest.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
